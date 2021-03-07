88bca0e94785bf3de899df53b7c390fffe4d2cee
Niek Palm <npalm@users.noreply.github.com>
2021-03-07 18:52:27 +0100

    feat: add support for non linux line breaks (#21)

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ async function run(): Promise<void> {
     const readmeFile: string = core.getInput('readme', { required: true });
     const tocLevel: number = parseInt(core.getInput('tocLevel', { required: true }));
     const actionFile: string = core.getInput('actionFile', { required: true });
+    const lineBreaks = core.getInput('readme', { required: true }) as 'LF' | 'CR' | 'CRLF';
 
     await generateActionMarkdownDocs({
       actionFile,
       readmeFile,
       updateReadme: true,
       tocLevel,
+      lineBreaks,
     });
   } catch (error) {
     core.debug(error);
