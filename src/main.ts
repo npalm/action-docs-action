166653ac226177ce277fa6d771e111927c7ac8e7
Niek Palm <npalm@users.noreply.github.com>
2021-10-14 23:13:55 +0200

    fix: upgrade dependencies (#131)

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function run(): Promise<void> {
       tocLevel,
       lineBreaks,
     });
-  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     core.debug(error);
     core.setFailed(error.message);
   }
